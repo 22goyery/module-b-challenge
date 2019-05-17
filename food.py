f4c9387aad74e121fc7c676f3cfaa6dc7684f847
22goyery <49483199+22goyery@users.noreply.github.com>
2019-05-17 10:38:53 -0400

    Add files via upload

---
+++ food.py
@@ -0,0 +1,35 @@
+def main()
+      for food in
+      print ("waht  to print")
+      print("what is your first name")
+    print("how old are you?")
+      for food in ("pizza ,appples,")
+      print("what to print a pizza")
+      print("what is your last name")
+       print("you,re at least 8030 days old.")
+     for food in 
+     print("what is the mac and cheese")
+     print("hello, george washington")
+     print("you're at leats 1144 weeks old")
+     for food in
+     print("what is  the meatballs)
+    print("what is yor middle name")
+    print("you're at least 264 months old)
+     
+     for a noun in
+     print("what is a pillow")
+     for a noun in pillow
+     print("what is a whirl")
+     for a noun in fancy
+     print("what is a stadium")
+     for a noun in a fancy pillow and whirl it at the stadium
+     print("taks your fancy pillow and whirl it at the stadium")
+
+
+     #the end of the program
+
+     main()
+
+
+
+     
